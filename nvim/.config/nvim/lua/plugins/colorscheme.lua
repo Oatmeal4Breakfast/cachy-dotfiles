d3d76ca5e6055e00c5565d727b16ecc19abfa0fe
Oatmeal4Breakfast <elvin.salcedo17@gmail.com>
2026-09-01 16:12:44 -0400

    chore: add nvim config for ruff and colorscheme

---
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {
+      theme = "wave",
+      background = {
+        dark = "wave",
+      },
+    },
+  },
+
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "kanagawa-wave",
+    },
+  },
+}
